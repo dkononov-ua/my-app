2c6446f03b7f7d2aa1e80084a76c035b77941b56
Denys <dkononov.ua@gmail.com>
2023-02-07 16:08:30 +0200

    first

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+
+const routes: Routes = [
+  {path: 'home' , component: HomeComponent},
+  {path: '' , redirectTo: 'home' , pathMatch: 'full'},
+  {path: 'page1' , component: Page1Component},
+  {path: '' , redirectTo: 'home' , pathMatch: 'full'},
+  {path: 'page2' , component: Page2Component},
+  {path: '' , redirectTo: 'home' , pathMatch: 'full'},
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    Page1Component,
+    Page2Component,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
